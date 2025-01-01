445a0d429a3091b877c9a3fdaeb88165ceeb1770
AnkitYadav9695 <22bcs11046@cuchd.in>
2025-01-01 23:05:34 +0530

    started adding basic redux detup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// compnent import
+import { Toaster } from 'react-hot-toast';
+import App from './App.jsx';
+//CSS import
+import './index.css';
+//Library import
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')).render(
-  
-    <App />
+  <Provider>
+      <BrowserRouter>
+         <App />
+         <Toaster/>
+      </BrowserRouter>
+  </Provider>
   
 )
