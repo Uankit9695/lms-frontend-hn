549e0038494635188361d4d91ea2be3ae34eb917
AnkitYadav9695 <22bcs11046@cuchd.in>
2025-08-09 04:58:41 +0530

    adding toster

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+//Component import
+import { Toaster } from "react-hot-toast";
+import App from "./App.jsx";
+// css import
+import "./index.css";
+//libaray import
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
- 
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-  
+    <Toaster />
+  </BrowserRouter>
 );
